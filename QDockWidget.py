efa6f3fb2faf6e474d1987b61ebe18569373ad50
Berkant Zor <berkantzor060@gmail.com>
2023-08-20 00:42:06 +0300

    Create QDockWidget.py

---
+++ QDockWidget.py
@@ -0,0 +1,30 @@
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+from PyQt5.QtCore import *
+
+
+class main(QMainWindow):#use for make a main window -same thing qwidget but some extra properties
+    def __init__(self):
+        super().__init__()
+        
+        self.central = QWidget()
+        
+        self.dock = QDockWidget()
+        self.addDockWidget(Qt.LeftDockWidgetArea,self.dock,Qt.Vertical)
+        self.dock.setWidget(QScrollArea())
+        
+        self.dock.setWindowTitle("Dock Widget")
+        
+        self.dock.setFeatures(QDockWidget.DockWidgetClosable | QDockWidget.DockWidgetMovable | QDockWidget.DockWidgetFloatable)
+        
+        # self.dock.setFloating(True)
+        
+        self.setCentralWidget(self.central)
+        
+        
+        self.resize(800,500)
+        self.show()
+        
+app = QApplication([])
+window = main()
+app.exec()
